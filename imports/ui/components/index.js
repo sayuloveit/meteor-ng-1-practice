f38f7e80310d30590d01a2cb92271cfb8d133008
Vic Zhu <vicxizhu@gmail.com>
2016-07-12 17:19:49 -0400

    add routing from ui router

--- imports/ui/components/index.js
+++ imports/ui/components/index.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
 import todosList from './todosList/todosList';
 import navigation from './navigation/navigation';
 
@@ -11,10 +12,21 @@ const name = 'mainApp'
 
 export default angular.module(name, [
   angularMeteor,
+  uiRouter,
   todosList.name,
   navigation.name
 ])
   .component(name, {
     templateUrl: 'imports/ui/components/index.html',
     controller: MainAppCtrl
-  });
+  })
+  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+      $locationProvider.html5Mode(true).hashPrefix('!');
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+          .state('home', {
+              url: '/',
+              template: '<main-app></main-app>'
+          });
+  }]);
